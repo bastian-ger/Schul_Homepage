10e5888584eab3667a0edb77a19849417189e982
basti <bastian-ger@users.noreply.github.com>
2018-01-18 14:37:15 +0100

    Created handlebars template for index.html
    Changed articles array to json.

--- js/home.js
+++ js/home.js
@@ -1,24 +1,44 @@
-// generator for short articles
 $(document).ready( () => {
-  let htmlString = '';
-  let i;
-  let wholeText = '';
-  let shortText = '';
-  const length = articles.length;
-  for (i = length - 1; i > length - 6; i--) {
-    wholeText = articles[i].aText;
-    shortText = wholeText.slice(0, 250);
-    htmlString =
-    `<article class='row mb-4 ml-1 mr-1'>
-      <div class='col-md-4 order-12 order-md-1'>
-        <img class='img-fluid p-1' src="${articles[i].images[0].img}" alt="${articles[i].images[0].imgAlt}">
-        <figcaption>${articles[i].images[0].imgCaption}</figcaption>
-      </div>
-      <div class='col-md-8 order-1 order-md-12'>
-        <h2>${articles[i].hl}</h2>  <span>${articles[i].date}</span>
-        <p>${shortText}<a href="${articles[i].url}">... weiterlesen</a></p>
-      </div>
-    </article>`;
-    $('#articles').append(htmlString);
+  // Get the template data from the HTML
+  let template = $('#handlebars-home').html();
+
+  // Gets first 400 letters of a string
+  Handlebars.registerHelper('shortText', (text, options) => {
+    return text.slice(0, 400);
+  });
+
+  // Shows only the first element of images array
+  Handlebars.registerHelper('arrayOnlyFirst', (array, options) => {
+    return options.fn(array[0]);
+  });
+
+  // Shows only the last five elements of an array
+  Handlebars.registerHelper('arrayOnlyFive', (array, options) => {
+  let ret = "";
+  if (array.length < 5) {
+    for (let i = array.length - 1; i > -1; i--) {
+      ret += options.fn(array[i]);
+    }
   }
+  else {
+    for (let j = array.length - 1; j > array.length - 6; j--) {
+      ret += options.fn(array[j]);
+    }
+  }
+  return ret;
+  });
+
+  // Gets index of arrays which work without #each
+  Handlebars.registerHelper('getIndex', (object, array) => {
+    return array.indexOf(object);
+  })
+
+  // Compile the template data into a function
+  let templateScript = Handlebars.compile(template);
+
+  // getting data from json file
+  const html = templateScript(contextArticles);
+
+  // Insert the HTML code into the page
+  $('#articles').append(html);
 });
