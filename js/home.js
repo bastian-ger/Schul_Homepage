3b2ed353ea985b7b799e673b81ee560269759cdd
basti <bastian-ger@users.noreply.github.com>
2018-01-23 19:40:39 +0100

    Added weather report on index.html
    openweathermap api

--- js/home.js
+++ js/home.js
@@ -41,4 +41,38 @@ $(document).ready( () => {
 
   // Insert the HTML code into the page
   $('#articles').append(html);
+
+  // Request for open weather map
+  const key = owm_key;
+  const url = `https://api.openweathermap.org/data/2.5/weather?id=6691073&units=metric&lang=de&APPID=${key}`;
+
+  $.ajax(url)
+    .done(addWeather)
+    .fail( (error) => requestError(error, 'Wetterbericht'));
+
+  function addWeather(data) {
+    let htmlString = '';
+    if (data.weather[0] && data.main && data.wind) {
+      const icon = data.weather[0].icon;
+      htmlString = `
+        <h4>Das aktuelle Wetter:</h4>
+        <img id="weather-icon" src="https://openweathermap.org/img/w/${icon}.png" alt="aktuelles Wetter">
+        <p>${data.weather[0].description}</p>
+        <p>Temperatur: ${data.main.temp} °C</p>
+        <p>Luftdruck: ${data.main.pressure} hPa</p>
+        <p>Luftfeuchtigkeit: ${data.main.humidity} %</p>
+        <p>Wind Speed: ${data.wind.speed} m/sek</p>
+        <p class="attribution">Dieser Wetterbericht wird geliefert von <a href="https://openweathermap.org/" target="_blank">OpenWeatherMap</a>.</p>
+        <p class="attribution">Lizenz: <a href="https://creativecommons.org/licenses/by-sa/4.0/deed.de" target="_blank">Creative Commons Attribution-ShareAlike 4.0 International</a></p>`
+    }
+    else {
+      htmlString = '<p>Es konnten keine aktuellen Wetterdaten geladen werden.</p>';
+    }
+    $('#weather').append(htmlString)
+  }
+
+  function requestError(error, brokenElement) {
+    console.log(error);
+    $('#weather').append(`Der ${brokenElement} konnte nicht geladen werden!`);
+  }
 });
