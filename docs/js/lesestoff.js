6a65ba9ebd666077c2d2cc052b5341dbafc6fff3
basti <bastian-ger@users.noreply.github.com>
2018-04-29 21:00:29 +0200

    file without babel transpile

--- docs/js/lesestoff.js
+++ docs/js/lesestoff.js
@@ -1,19 +1,16 @@
-'use strict';
-
-$(document).ready(function () {
+$(document).ready( () => {
   // Request for NY Times Books API
-  var key = nyt_key;
-  var selection = void 0,
-      url = void 0;
-  var htmlString = '';
+  const key = nyt_key;
+  let selection, url;
+  let htmlString = '';
 
-  $('#select-bs-list').change(function (event) {
+  $('#select-bs-list').change( event => {
     selection = event.target.value;
-    url = 'http://api.nytimes.com/svc/books/v3/lists/current/' + selection + '.json?api-key=' + key;
+    url = `http://api.nytimes.com/svc/books/v3/lists/current/${selection}.json?api-key=${key}`;
 
-    $.ajax(url).then(addBooks).fail(function (error) {
-      return requestError(error, selection);
-    });
+    $.ajax(url)
+      .then(addBooks)
+      .fail( (error) => requestError(error, selection));
   });
 
   function addBooks(data) {
@@ -22,10 +19,22 @@ $(document).ready(function () {
       $('#list-children').empty();
     }
     if (data.results && data.results.books && data.results.books.length > 1) {
-      htmlString = data.results.books.map(function (book) {
-        return '<article class="row mb-3 bg-light">\n          <div class="col-sm-9 p-3">\n            <h5>Rang: ' + book.rank + ' <small>(Vorwoche: ' + book.rank_last_week + '</small>)</h5>\n            <p>Wochen auf der Liste: ' + book.weeks_on_list + '</p>\n            <h4>Titel: ' + book.title + '</h4>\n            <p>Autor: ' + book.author + '</p>\n            <p>Verlag: ' + book.publisher + '</p>\n            <p>Beschreibung: ' + book.description + '</p>\n          </div>\n          <div class="col-sm-3 p-3">\n            <img class="img-fluid" src="' + book.book_image + '" alt="Buchcover">\n          </div>\n        </article>';
-      }).join('');
-    } else {
+      htmlString = data.results.books.map( book =>
+        `<article class="row mb-3 bg-light">
+          <div class="col-sm-9 p-3">
+            <h5>Rang: ${book.rank} <small>(Vorwoche: ${book.rank_last_week}</small>)</h5>
+            <p>Wochen auf der Liste: ${book.weeks_on_list}</p>
+            <h4>Titel: ${book.title}</h4>
+            <p>Autor: ${book.author}</p>
+            <p>Verlag: ${book.publisher}</p>
+            <p>Beschreibung: ${book.description}</p>
+          </div>
+          <div class="col-sm-3 p-3">
+            <img class="img-fluid" src="${book.book_image}" alt="Buchcover">
+          </div>
+        </article>`).join('');
+    }
+    else {
       htmlString = '<p>Leider ist die Anfrage fehlgeschlagen.</p>';
     }
 
@@ -33,6 +42,6 @@ $(document).ready(function () {
   }
 
   function requestError(error, brokenItem) {
-    $('#list-children').append('<p>Leider ist die Anzeige der Liste ' + brokenItem + ' fehlgeschlagen.</p>');
+    $('#list-children').append(`<p>Leider ist die Anzeige der Liste ${brokenItem} fehlgeschlagen.</p>`);
   }
 });
