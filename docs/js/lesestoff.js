e45741069d1884965fdb88b4f3d4a00eb8a519ef
basti <bastian-ger@users.noreply.github.com>
2018-04-29 20:48:21 +0200

    file without uglify

--- docs/js/lesestoff.js
+++ docs/js/lesestoff.js
@@ -1,3 +1,38 @@
-"use strict";$(document).ready(function(){
-// Request for NY Times Books API
-var n=nyt_key,s=void 0,i=void 0,t="";function l(e){0<t.length&&(t="",$("#list-children").empty()),t=e.results&&e.results.books&&1<e.results.books.length?e.results.books.map(function(e){return'<article class="row mb-3 bg-light">\n          <div class="col-sm-9 p-3">\n            <h5>Rang: '+e.rank+" <small>(Vorwoche: "+e.rank_last_week+"</small>)</h5>\n            <p>Wochen auf der Liste: "+e.weeks_on_list+"</p>\n            <h4>Titel: "+e.title+"</h4>\n            <p>Autor: "+e.author+"</p>\n            <p>Verlag: "+e.publisher+"</p>\n            <p>Beschreibung: "+e.description+'</p>\n          </div>\n          <div class="col-sm-3 p-3">\n            <img class="img-fluid" src="'+e.book_image+'" alt="Buchcover">\n          </div>\n        </article>'}).join(""):"<p>Leider ist die Anfrage fehlgeschlagen.</p>",$("#list-children").append(t)}$("#select-bs-list").change(function(e){s=e.target.value,i="http://api.nytimes.com/svc/books/v3/lists/current/"+s+".json?api-key="+n,$.ajax(i).then(l).fail(function(e){return n=s,void $("#list-children").append("<p>Leider ist die Anzeige der Liste "+n+" fehlgeschlagen.</p>");var n})})});
+'use strict';
+
+$(document).ready(function () {
+  // Request for NY Times Books API
+  var key = nyt_key;
+  var selection = void 0,
+      url = void 0;
+  var htmlString = '';
+
+  $('#select-bs-list').change(function (event) {
+    selection = event.target.value;
+    url = 'http://api.nytimes.com/svc/books/v3/lists/current/' + selection + '.json?api-key=' + key;
+
+    $.ajax(url).then(addBooks).fail(function (error) {
+      return requestError(error, selection);
+    });
+  });
+
+  function addBooks(data) {
+    if (htmlString.length > 0) {
+      htmlString = '';
+      $('#list-children').empty();
+    }
+    if (data.results && data.results.books && data.results.books.length > 1) {
+      htmlString = data.results.books.map(function (book) {
+        return '<article class="row mb-3 bg-light">\n          <div class="col-sm-9 p-3">\n            <h5>Rang: ' + book.rank + ' <small>(Vorwoche: ' + book.rank_last_week + '</small>)</h5>\n            <p>Wochen auf der Liste: ' + book.weeks_on_list + '</p>\n            <h4>Titel: ' + book.title + '</h4>\n            <p>Autor: ' + book.author + '</p>\n            <p>Verlag: ' + book.publisher + '</p>\n            <p>Beschreibung: ' + book.description + '</p>\n          </div>\n          <div class="col-sm-3 p-3">\n            <img class="img-fluid" src="' + book.book_image + '" alt="Buchcover">\n          </div>\n        </article>';
+      }).join('');
+    } else {
+      htmlString = '<p>Leider ist die Anfrage fehlgeschlagen.</p>';
+    }
+
+    $('#list-children').append(htmlString);
+  }
+
+  function requestError(error, brokenItem) {
+    $('#list-children').append('<p>Leider ist die Anzeige der Liste ' + brokenItem + ' fehlgeschlagen.</p>');
+  }
+});
